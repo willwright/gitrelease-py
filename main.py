62d54406a8e4354cfb26628100daa8a20b3625c9
Will Wright <wwrig@guidance.com>
2020-02-19 09:58:28 -0800

    JIRA syncing of FixVersion and branches checkout list branches from remote roll support for legacy git-release next support for legacy git-release

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python
 
 import api
+import config
 from git import Repo
+import helper
+import local
+import jira
 import os
 import sh
 import subprocess
 import sys
-import config
-from config import incrementCandidate, clearbranches, add
-import helper
-from jira import parse_jira_key
 
 repo = Repo(os.getcwd())
 assert not repo.bare, "This directory is a bare repo, please clone a project to work with"
 
 
-def jira_sync():
+def jira_sync_down():
     releases_dict = config.read_config()
 
     # Get all issues in project/version
@@ -23,10 +23,86 @@ def jira_sync():
 
     # Add any missing branches
     for issue in issues_list:
+        found = False
         for branch in releases_dict["branches"]:
-            if helper.key_branch_comp(issue, branch):
-                print(issue + " not in branches")
+            if issue in branch:
+                print("{}: found!".format(issue))
+                found = True
 
+        if not found:
+            print("{} is missing!".format(issue))
+            print()
+            print("0: Add to local release")
+            print("1: Remove from JIRA release")
+            print("2: Skip")
+            choice = input("Selection: [0]".format(issue)) or 0
+            print()
+
+            if int(choice) == 0:
+                branches = helper.find_branch_by_query(issue)
+                if len(branches) <0:
+                    print("No branches found matching your query")
+                    print()
+                    continue
+
+                for i in range(0, len(branches)):
+                    print("{option}: {branch}".format(option=i, branch=branches[i]))
+
+                chosen_branch = input("Select Branch (x = cancel): ") or "x"
+                if chosen_branch.lower() == "x" or chosen_branch == "":
+                    print()
+                    continue
+
+                chosen_branch = int(chosen_branch)
+
+                print("Selected: {branch}".format(branch=branches[chosen_branch]))
+                print()
+                releases_dict["branches"].append(branches[chosen_branch])
+
+            elif int(choice) == 1:
+                api.jira_delete_fixversion(issue, releases_dict["version"])
+                print("Removed {} from {}".format(issue, releases_dict["version"]))
+                print()
+            elif int(choice) == 2:
+                continue
+            else:
+                continue
+
+    config.write_config(releases_dict)
+    return
+
+
+def jira_sync_up():
+    releases_dict = config.read_config()
+
+    # Get all issues in project/version
+    issues_list = api.jira_search_issues(releases_dict["projectslug"], releases_dict["version"])
+
+    # Add any missing branches
+    for branch in releases_dict["branches"]:
+        found = False
+        for issue in issues_list:
+            if jira.parse_jira_key(branch) in issue:
+                print("{}: found!".format(branch))
+                found = True
+        if not found:
+            print("{} is missing!".format(branch))
+            print()
+            print("0: Add to JIRA release")
+            print("1: Remove from local release")
+            print("2: Skip")
+            choice = input("Selection: [0]".format(branch)) or 0
+            print()
+            if int(choice) == 0:
+                api.jira_create_fixveresion(jira.parse_jira_key(branch), releases_dict["version"])
+            elif int(choice) == 1:
+                releases_dict["branches"].remove(branch)
+                continue
+            elif int(choice) == 2:
+                continue
+            else:
+                continue
+    config.write_config(releases_dict)
     return
 
 
@@ -54,8 +130,8 @@ def rm():
         jira_send = False
 
     if jira_send:
-        jira_key = parse_jira_key(release_dict["branches"][choice])
-        api.zapier_delete_fixversion(jira_key, release_dict["version"])
+        jira_key = jira.parse_jira_key(release_dict["branches"][choice])
+        api.jira_delete_fixversion(jira_key, release_dict["version"])
 
     # Remove the branch from the Dictionary
     del release_dict["branches"][choice]
@@ -69,7 +145,7 @@ def rm():
 def feature():
     branch = find_feature()
     if branch:
-        add(branch)
+        config.add(branch)
         print("Send to JIRA [yes]? yes/no")
         jira_send = bool(input()) or "yes"
         if jira_send == "yes":
@@ -80,9 +156,9 @@ def feature():
             jira_send = False
 
         if jira_send:
-            jira_key = parse_jira_key(branch)
+            jira_key = jira.parse_jira_key(branch)
             release_dict = config.read_config()
-            api.zapier_create_fixveresion(jira_key, release_dict["version"])
+            api.jira_create_fixveresion(jira_key, release_dict["version"])
 
     return
 
@@ -105,10 +181,7 @@ def find_feature():
     elif search_type == 3:
         feature_query = "hotfix/"
 
-    result = subprocess.run(['git', 'branch', '-a'], stdout=subprocess.PIPE)
-    branches = result.stdout.decode('utf-8').splitlines()
-    branches = list(map(lambda x: x.strip(), branches))
-    branches = list(filter(lambda x: feature_query.lower() in x.lower(), branches))
+    branches = helper.find_branch_by_query(feature_query)
 
     assert len(branches) > 0, "No branches found matching your query"
 
@@ -122,7 +195,7 @@ def find_feature():
     chosen_branch = int(chosen_branch)
 
     print("Selected: {branch}".format(branch=branches[chosen_branch]))
-    return branches[chosen_branch]
+    return branches[chosen_branch].replace("remotes/", "", 1)
 
 
 def init():
@@ -155,7 +228,7 @@ def init():
     status()
     choice = input("Clear branches [yes/no] or inherit from current config? [no]") or "no"
     if choice != "no":
-        clearbranches()
+        config.clearbranches()
 
     # Write release to config
     config.write_config(release_dict)
@@ -167,43 +240,66 @@ def init():
 def checkout():
     release_dict = config.read_config()
 
-    print("Checkout")
-    if release_dict["projectslug"]:
-        projectslug = input("Choose a projectslug [{}]: ".format(release_dict["projectslug"])) or release_dict["projectslug"]
-    else:
-        projectslug = input("Choose a projectslug: ")
-
-    release_dict["projectslug"] = projectslug.strip()
-    print()
-
-    # Read all the releases for projectslug
-    items_dict = api.readproject(release_dict["projectslug"])
-    items_list = sorted(items_dict["Items"], key=get_key, reverse=False)
-    for key in range(0, len(items_list)):
-        print("{}: release-v{}-rc{}".format(key, items_list[key]["version"], items_list[key]["candidate"]))
-
-    items_list_key = int(input("Choose release: "))
-    release_dict = items_list[items_list_key]
-
-    release_dict.pop("projectslug#version#candidate")
-    config.write_config(release_dict)
-
     sh.git.fetch("--all")
-    sh.git.checkout("release-v{}-rc{}".format(release_dict["version"], release_dict["candidate"]))
+    print("Getting Release Branches...")
+    branches_list = helper.find_branch_by_query("origin/release-v")
+
+    if len(branches_list) <= 0:
+        print("No Release Branches found in repo")
+
+    # Remove "remotes/" from the beginning of the branch name
+    branches_list = map(lambda x: x.replace("remotes/", "", 1), branches_list)
+
+    # Sort the branches by version, candidate
+    branches_list = sorted(branches_list, key=helper.release_branch_comp, reverse=False)
+    for key in range(0, len(branches_list)):
+        print("{}: {}".format(key, branches_list[key]))
+
+    choice = input("Choose release (x = cancel): ") or "x"
+    if choice.strip().lower() == "x":
+        return
+    else:
+        choice = int(choice)
+
+    choice_branch = branches_list[choice]
+    sh.git.checkout(choice_branch.replace("origin/", "", 1))
+
+    # TODO: Update release_dict branches from one of these two sources
+    if helper.use_api_share():
+        # Update release_dict from the API
+        raw_candidate = api.read_candidate(release_dict)
+        if "Item" in raw_candidate:
+            release_dict = raw_candidate["Item"]
+            release_dict.pop("projectslug#version#candidate")
+    else:
+        release_dict["branches"] = local.read_git_release(release_dict["version"])
+
+    config.write_config(release_dict)
 
 
 def roll():
-    nextReleaseCandidate = get_next_release_candidate()
+    releases_dict = config.read_config()
 
     sh.git.fetch("--all")
 
-    print("Creating " + nextReleaseCandidate + " ...")
+    print("Creating " + helper.get_next_release_candidate() + " ...")
 
     # Change this so that "master" is configurable
     try:
-        sh.git.checkout("-b", nextReleaseCandidate, "master", _err=sys.stderr)
-        incrementCandidate()
-        config.write_config()
+        try:
+            sh.git.checkout("-b", helper.get_next_release_candidate(), "master", _err=sys.stderr, _out=sys.stdout)
+        except sh.ErrorReturnCode_1:
+            return
+
+        releases_dict["candidate"] = int(releases_dict["candidate"]) + 1
+        config.write_config(releases_dict)
+        # TODO: Write config to API or Local
+        if helper.use_api_share():
+            pass
+        else:
+            local.write_git_release(releases_dict["version"], releases_dict["branches"])
+            sh.git.add("releases/release-v{}".format(releases_dict["version"]))
+            sh.git.commit("-m", "Appending Release Branch Definition file")
     except sh.ErrorReturnCode_128:
         sys.exit()
 
@@ -212,43 +308,48 @@ def roll():
 
     merge_branches(branches)
 
-    hasConflicts = find_conflicts()
-    if hasConflicts:
+    has_conflicts = find_conflicts()
+    if has_conflicts:
         print("Not pushing to origin")
     else:
         print("Pushing to origin")
-        print("git push origin " + nextReleaseCandidate)
-        sh.git.push("origin", nextReleaseCandidate)
+        print("Pushing to origin")
+        print("git push origin " + helper.get_current_release_candidate())
+        #sh.git.push("origin", helper.get_current_release_candidate())
 
 
 def next():
-    current_release_candidate = get_current_release_candidate()
-    nextReleaseCandidate = get_next_release_candidate()
+    releases_dict = config.read_config()
 
     sh.git.fetch("--all")
 
-    print("Creating " + nextReleaseCandidate + " ...")
+    print("Creating " + helper.get_next_release_candidate() + " ...")
 
     # Change this so that "master" is configurable
     try:
-        sh.git.checkout("-b", nextReleaseCandidate, current_release_candidate, _err=sys.stderr)
-        incrementCandidate()
-        config.write_config()
-    except sh.ErrorReturnCode_128:
+        sh.git.checkout("-b", helper.get_next_release_candidate(), helper.get_current_release_candidate(), _err=sys.stderr)
+        releases_dict["candidate"] = int(releases_dict["candidate"]) + 1
+        config.write_config(releases_dict)
+        # TODO: Write config to API or Local
+        if helper.use_api_share():
+            pass
+        else:
+            local.write_git_release(releases_dict["version"], releases_dict["branches"])
+            sh.git.add("releases/release-v{}".format(releases_dict["version"]))
+            sh.git.commit("-m", "Appending Release Branch Definition file")
+    except sh.ErrorReturnCode_128 as err:
+        print(err)
         sys.exit()
 
-    result = subprocess.run(['git', 'config', '--get-all', 'releases.branches'], stdout=subprocess.PIPE)
-    branches = result.stdout.decode('utf-8').splitlines()
+    merge_branches(releases_dict["branches"])
 
-    merge_branches(branches)
-
-    hasConflicts = find_conflicts()
-    if hasConflicts:
+    has_conflicts = find_conflicts()
+    if has_conflicts:
         print("Not pushing to origin")
     else:
         print("Pushing to origin")
-        print("git push origin " + nextReleaseCandidate)
-        #sh.git.push("origin", nextReleaseCandidate)
+        print("git push origin " + helper.get_current_release_candidate())
+        #sh.git.push("origin", helper.get_current_release_candidate())
 
 
 def status():
@@ -263,6 +364,7 @@ def status():
 
     return
 
+
 def merge_branches(branches):
     sh.git.fetch("--all")
 
@@ -274,9 +376,13 @@ def merge_branches(branches):
             sh.git.merge("--no-ff", "--no-edit", branch, _err=sys.stderr, _out=sys.stdout)
         except sh.ErrorReturnCode_1:
             return
+        except sh.ErrorReturnCode_128:
+            continue
+    return
 
 
 def find_conflicts():
+    print()
     print("Looking for conflicts with merge.")
     result = subprocess.run(['git', 'diff', '--name-only', '--diff-filter=U'], stdout=subprocess.PIPE)
     conflicts = result.stdout.decode('utf-8')

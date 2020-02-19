7be9895b135893f635b7b529bd2d73ec31ca870c
Will Wright <wwrig@guidance.com>
2020-02-19 09:57:25 -0800

    new class for read/writing to "releases/*" for git-release legacy support

---
+++ local.py
@@ -0,0 +1,30 @@
+
+
+def read_git_release(version):
+    branches_list = []
+    with open("releases/release-v{}".format(version), "r") as stream:
+        try:
+            for line in stream:
+                line = line.strip()
+                if len(line) > 0:
+                    branches_list.append(line)
+        except:
+            pass
+        finally:
+            stream.close()
+
+    return branches_list
+
+
+def write_git_release(version, branches):
+    with open("releases/release-v{}".format(version), "w") as stream:
+        try:
+            stream.writelines(map(lambda x: "\n" + x, branches))
+            # Write one last linebreak to be compatible for legacy git-release
+            stream.write("\n")
+        except:
+            pass
+        finally:
+            stream.close()
+
+    return

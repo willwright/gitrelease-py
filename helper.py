9a381097650de7c0e234cba4e14e4ff95c6a4ce6
Will Wright <wwrig@guidance.com>
2020-02-19 09:56:51 -0800

    release branch human sorting

--- helper.py
+++ helper.py
@@ -1,31 +1,66 @@
+import subprocess
+
 import config
+import os
 import jira
 import re
+import yaml
 
 def get_current_release_candidate():
     releases_dict = config.read_config()
 
-    return "release-v{}-rc{candidate}".format(releases_dict["version"], releases_dict["candidate"])
+    return "release-v{}-rc{}".format(releases_dict["version"], releases_dict["candidate"])
 
 
 def get_next_release_candidate():
     releases_dict = config.read_config()
 
-    return "release-v{}-rc{candidate}".format(releases_dict["version"], int(releases_dict["candidate"]) + 1)
+    return "release-v{}-rc{}".format(releases_dict["version"], int(releases_dict["candidate"]) + 1)
 
 
-def get_key(item_dict):
+def release_sort_hash(version, candidate):
     key = 0
-    for part in item_dict["version"].split("."):
+    for part in version.split("."):
         key += int(part)*100
 
-    key += int(item_dict["candidate"])
+    key += int(candidate)
+
     return key
 
 
-def key_branch_comp(key, branch):
-    key_part = re.search("\d+", key).group()
-    branch_part = jira.parse_jira_key(branch)
-    branch_part = re.search("\d+", branch_part).group()
+def get_key(item_dict):
+    version = item_dict["version"]
+    candidate = item_dict["candidate"]
 
-    return key_part in branch_part
+    return release_sort_hash(version, candidate)
+
+
+def release_branch_comp(branch):
+    branch.replace("remotes/origin/", "")
+    key = 0
+
+    regex = re.search("[\d+\.]+\d+", branch)
+    version = regex.group()
+
+    regex = re.search("\d+$", branch)
+    candidate = regex.group()
+
+    return release_sort_hash(version, candidate)
+
+
+def find_branch_by_query(query):
+    result = subprocess.run(['git', 'branch', '-a'], stdout=subprocess.PIPE)
+    branches = result.stdout.decode('utf-8').splitlines()
+    branches = list(map(lambda x: x.strip(), branches))
+    branches = list(filter(lambda x: query.lower() in x.lower(), branches))
+    return branches
+
+
+def use_api_share():
+    with open("gitrelease.yaml", "r") as stream:
+        try:
+            config_dict = yaml.safe_load(stream)
+        except yaml.YAMLError as err:
+            print(err)
+
+    return bool(config_dict["useApiShare"]) or False
